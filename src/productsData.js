58679358c09f7f384794f8dc72ab0cbd3fe0f89c
Marwan <dragonkingofhell@gmail.com>
2021-06-04 02:56:49 +0200

    added the product using for loop

---
+++ src/productsData.js
@@ -0,0 +1,44 @@
+export default function product(){
+
+  return [
+    {
+      text1: "Card 1",
+      text2: "Some text",
+    },
+  
+    {
+      text1: "heloo2",
+      text2: "Some text222",
+    },
+  
+    {
+      text1: "Card333",
+      text2: "Some333",
+    },
+
+    {
+      text1: "Card333",
+      text2: "Some333",
+    },
+
+    {
+      text1: "Card333",
+      text2: "Some333",
+    },
+
+    
+  ];
+}
+
+
+function test(){
+  const tes = [
+    {
+      id: "",
+      prodName: "",
+      prodImg: "",
+      prodPrice: "",
+      addButton: "",
+    },
+  ]
+}
